8011bc8e15e30ebbb6da105e4968389ae2be7e48
unknown <forestwwk@gmail.com>
2017-12-15 12:32:13 +0800

    Update db

--- util/db.go
+++ util/db.go
@@ -19,7 +19,7 @@ func GetExecutePath() string {
 	return dir
 }
 
-func SetDB(path ...string) {
+func SetDB_old(path ...string) {
 	if path != nil {
 		dbPath = path[0]
 	} else {
@@ -29,7 +29,7 @@ func SetDB(path ...string) {
 }
 
 // GetDB Get the DB connection
-func GetDB() *sql.DB {
+func GetDB_old() *sql.DB {
 	// log.Println("DB_PATH ", dbPath)
 	if len(dbPath) == 0 {
 		SetDB()
@@ -40,6 +40,18 @@ func GetDB() *sql.DB {
 	if err != nil {
 		log.Fatal(err, "dbPath = ", dbPath)
 		Error.Fatal(err, "dbPath = ", dbPath)
+	}
+
+	return db
+}
+
+
+func GetDB() *sql.DB {
+    var db *sql.DB
+    db, err := sql.Open("postgres", "host=ec2-23-21-189-181.compute-1.amazonaws.com port=5432 user=rvxfododvigsgj password=fb71ac2a80b2877ba7b4a3114fa4c6b4fa8bdaba6c34a15f0faf6edc07ddbec3 dbname=d9flf74p9len8l sslmode=disable")
+    if err != nil {
+		log.Fatal(err, "Fail to connect DB")
+		Error.Fatal(err, "Fail to connect DB")
 	}
 
 	return db
