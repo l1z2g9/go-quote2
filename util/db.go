bb0ddb82d20f3cc2580c30e223567025636fc7e5
unknown <forestwwk@gmail.com>
2017-12-15 16:16:19 +0800

    Update db

--- util/db.go
+++ util/db.go
@@ -48,8 +48,6 @@ func GetDB_old() *sql.DB {
 
 
 func GetDB() *sql.DB {
-		log.Fatal("try to connect DB")
-		Error.Fatal("try to connect DB")
 fmt.Println("try to connect DB 222 " + os.Getenv("DATABASE_URL"))
     var db *sql.DB
     //db, err := sql.Open("postgres", "host=ec2-23-21-189-181.compute-1.amazonaws.com port=5432 user=rvxfododvigsgj password=fb71ac2a80b2877ba7b4a3114fa4c6b4fa8bdaba6c34a15f0faf6edc07ddbec3 dbname=d9flf74p9len8l sslmode=disable")
@@ -59,12 +57,12 @@ fmt.Println("try to connect DB 222 " + os.Getenv("DATABASE_URL"))
     if err != nil {
 fmt.Println("err " , err)
 		log.Fatal(err, "Fail to connect DB")
-		Error.Fatal(err, "Fail to connect DB")
+
 	}
 
     fmt.Println("Successfully connected!")
 
-log.Println("Get connection")
-Info.Println("Get connection")
+
+
 	return db
 }
