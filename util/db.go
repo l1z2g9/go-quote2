af4d72721727917fe016913c91de31a3424a33f5
unknown <forestwwk@gmail.com>
2017-12-15 15:29:30 +0800

    Update db

--- util/db.go
+++ util/db.go
@@ -54,8 +54,8 @@ fmt.Println("try to connect DB " + os.Getenv("DATABASE_URL"))
     var db *sql.DB
     //db, err := sql.Open("postgres", "host=ec2-23-21-189-181.compute-1.amazonaws.com port=5432 user=rvxfododvigsgj password=fb71ac2a80b2877ba7b4a3114fa4c6b4fa8bdaba6c34a15f0faf6edc07ddbec3 dbname=d9flf74p9len8l sslmode=disable")
      db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-    fmt.Println("db " + db)
-    fmt.Println("err ?? " + err)
+    fmt.Println(db)
+    fmt.Println(err)
     if err != nil {
 fmt.Println("err " , err)
 		log.Fatal(err, "Fail to connect DB")
